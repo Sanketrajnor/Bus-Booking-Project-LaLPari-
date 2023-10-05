06f3693984c9876d73f7d4c90a12fe8daba33497
Sanket Rajnor <107184559+Sanketrajnor@users.noreply.github.com>
2023-10-05 09:56:13 +0530

    Add files via upload

---
+++ LaLPari Project(HCI)/README.md
@@ -0,0 +1,22 @@
+# redBus-Clone
+
+## Project Overview
+A Front Page Clone of redBus.
+
+### How to run this Application?
+  * Download as .zip file or clone this project:
+    ```
+    $ git clone https://github.com/msgaurav/redBus-Clone
+    ```
+    * If downloaded, unzip and open **main.html** file in a browser.
+  - Or you can directly [Click here:point_up_2:](https://codepen.io/msgaurav/full/XQjPbE) to run this application.
+
+### Skills Used
+  * HTML5
+  * CSS3
+  * Bootstrap 4
+  * JavaScript
+  * jQuery
+  
+#### Note
+  * I do not own any copyrights, all the rights go to their respective owners. Thank You.:smiley:
---
+++ LaLPari Project(HCI)/js/app.js
@@ -0,0 +1,72 @@
+/* Input Search From Focus-In/Out */
+$(".inputForm").focusin(function(){
+	$(this).prev("label").addClass("inputLabel-focusIn");
+});
+
+// Input Search From Focus-Out */
+$(".inputForm").focusout(function(){
+	var inputForm = $(this).val();
+	if (inputForm.length > 0) {
+		$(this).prev("label").addClass("inputLabel-focusIn");
+	} else {
+		$(this).prev("label").removeClass("inputLabel-focusIn");
+	}
+});
+
+/* Input Onward/Reture Date Focus-In/Out */
+$("#input-label-onward-date, #input-label-return-date").focusin(function(){
+	$(this).attr("type", "date");
+});
+
+// Input Onward/Reture Date Focus-Out */
+$("#input-label-onward-date, #input-label-return-date").focusout(function(){
+	var inputFormDate = $(this).val();
+
+	if (inputFormDate.length > 0) {
+		$(this).attr("type", "date");
+	} else {
+		$(this).attr("type", "text");
+	}
+});
+
+/* Pass Input-From's Locations to Input field */
+// From Locations Data
+var inputFromList = [
+	{ fromLocation : "Gurgaon"},
+	{ fromLocation : "Delhi"},
+	{ fromLocation : "Jaipur"},
+];
+
+var optionFrom;
+var inputFromDatalist = $("#input-from-list");
+
+for (var i = 0; i < inputFromList.length; i++) {
+	optionFrom = $("<option></option>");
+	optionFrom.attr("value", inputFromList[i].fromLocation);
+	inputFromDatalist.append(optionFrom);
+}
+
+/* Pass Input-To's Locations to Input field */
+// To Locations Data
+var inputToList = [
+	{ toLocation : "Gurgaon"},
+	{ toLocation : "Delhi"},
+	{ toLocation : "Jaipur"}
+];
+
+var optionTo;
+var inputToDatalist = $("#input-to-list");
+
+for (var i = 0; i < inputToList.length; i++) {
+	optionTo = $("<option></option>");
+	optionTo.attr("value", inputToList[i].toLocation);
+	inputToDatalist.append(optionTo);
+}
+
+// Data Filter
+$("#input-label-from").change(function(){
+	var dataSelect = $(this).val();
+	console.log(dataSelect);
+
+	$("#input-to-list").find("option[value='"+dataSelect+"']").remove();
+});
